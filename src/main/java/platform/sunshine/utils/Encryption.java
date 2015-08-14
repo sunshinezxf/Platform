0598a2857fb0d7d1ef33713975dbd978e0233dee
zhangxufan <sunshinezxf@hotmail.com>
2015-08-14 16:32:55 +0800

    add des encryption

--- src/main/java/platform/sunshine/utils/Encryption.java
+++ src/main/java/platform/sunshine/utils/Encryption.java
@@ -1,6 +1,14 @@
 package platform.sunshine.utils;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import java.security.MessageDigest;
+import java.security.SecureRandom;
 
 /**
  * Created by sunshine on 2015/8/14.
@@ -26,5 +34,50 @@ public class Encryption {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public static String desEncode(String data, String key) throws Exception {
+        byte[] bt = encrypt(data.getBytes(), key.getBytes());
+        String strs = new BASE64Encoder().encode(bt);
+        return strs;
+    }
+
+    public static String desDecode(String data, String key) throws Exception {
+        if (data == null)
+            return null;
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] buf = decoder.decodeBuffer(data);
+        byte[] bt = decrypt(buf, key.getBytes());
+        return new String(bt);
+    }
+
+    private static byte[] decrypt(byte[] data, byte[] key) throws Exception {
+        SecureRandom sr = new SecureRandom();
+
+        DESKeySpec dks = new DESKeySpec(key);
+
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        Cipher cipher = Cipher.getInstance("DES");
+
+        cipher.init(Cipher.DECRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
+    }
+
+    private static byte[] encrypt(byte[] data, byte[] key) throws Exception {
+        SecureRandom sr = new SecureRandom();
+
+        DESKeySpec dks = new DESKeySpec(key);
+
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey securekey = keyFactory.generateSecret(dks);
+
+        Cipher cipher = Cipher.getInstance("DES");
+
+        cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+
+        return cipher.doFinal(data);
     }
 }
--- src/main/java/platform/sunshine/utils/IDGenerator.java
+++ src/main/java/platform/sunshine/utils/IDGenerator.java
@@ -1,7 +1,7 @@
 package platform.sunshine.utils;
 
 /**
- * Created by pandora on 2015/8/14.
+ * Created by sunshine on 2015/8/14.
  */
 public class IDGenerator {
     private static String prefix(Class T) {
@@ -9,15 +9,8 @@ public class IDGenerator {
         return prefix;
     }
 
-    private static String credit(int length) {
+    public static String generate(int length) {
         return Random.code(length);
     }
 
-    public static String generate(Class T, int length) {
-        return Encryption.md5(prefix(T) + credit(length));
-    }
-
-    public static void main(String[] args) {
-        System.out.println(generate(IDGenerator.class, 10));
-    }
 }
