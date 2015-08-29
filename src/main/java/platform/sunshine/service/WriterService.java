df8aa4447694ba1b248fa07c7c704757d66ab079
sunshine <zxfan11@software.nju.edu.cn>
2015-08-29 17:11:53 +0800

    add Writer service interface and its implement class

---
+++ src/main/java/platform/sunshine/service/WriterService.java
@@ -0,0 +1,7 @@
+package platform.sunshine.service;
+
+/**
+ * Created by sunshine on 15/8/29.
+ */
+public interface WriterService {
+}
---
+++ src/main/java/platform/sunshine/service/impl/WriterServiceImpl.java
@@ -0,0 +1,9 @@
+package platform.sunshine.service.impl;
+
+import platform.sunshine.service.WriterService;
+
+/**
+ * Created by sunshine on 15/8/29.
+ */
+public class WriterServiceImpl implements WriterService {
+}
