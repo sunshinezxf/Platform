b9689c7c91472bc5fc85be3e5668db4baecc359e
sunshine <zxfan11@software.nju.edu.cn>
2015-08-31 20:15:14 +0800

    add annotation of writerservice impl

--- src/main/java/platform/sunshine/service/impl/WriterServiceImpl.java
+++ src/main/java/platform/sunshine/service/impl/WriterServiceImpl.java
@@ -1,11 +1,13 @@
 package platform.sunshine.service.impl;
 
+import org.springframework.stereotype.Service;
 import platform.sunshine.service.WriterService;
 import platform.sunshine.utils.ResultData;
 
 /**
  * Created by sunshine on 15/8/29.
  */
+@Service
 public class WriterServiceImpl implements WriterService {
 
     public ResultData createWriter() {
