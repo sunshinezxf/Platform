3dce3f2336643c01557c0794408271d30b798458
sunshine <zxfan11@software.nju.edu.cn>
2015-09-04 17:14:48 +0800

    add account vo and add query writer mybatis sql

---
+++ src/main/java/platform/sunshine/vo/AccountVo.java
@@ -0,0 +1,10 @@
+package platform.sunshine.vo;
+
+/**
+ * Created by sunshine on 15/9/4.
+ */
+public class AccountVo {
+    private String accountId;
+    private String accountUsername;
+    private String accountEmail;
+}
