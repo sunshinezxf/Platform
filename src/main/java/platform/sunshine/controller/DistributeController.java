8c38f2078a4d79cd1915d619df7c5fc1ecbaae0d
sunshine <zxfan11@software.nju.edu.cn>
2015-09-13 14:56:12 +0800

    add article view for reader

--- src/main/java/platform/sunshine/controller/DistributeController.java
+++ src/main/java/platform/sunshine/controller/DistributeController.java
@@ -1,7 +1,12 @@
 package platform.sunshine.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by sunshine on 15/8/28.
  */
+@RestController
+@RequestMapping("distribute")
 public class DistributeController {
 }
