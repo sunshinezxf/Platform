716a20103918268b2b53b53b7dfcb131da69bd1d
sunshine <zxfan11@software.nju.edu.cn>
2015-09-21 09:28:26 +0800

    add method insert and query of reader

--- src/main/java/platform/sunshine/dao/ReaderDao.java
+++ src/main/java/platform/sunshine/dao/ReaderDao.java
@@ -1,7 +1,13 @@
 package platform.sunshine.dao;
 
+import platform.sunshine.model.Reader;
+import platform.sunshine.utils.ResultData;
+
 /**
  * Created by sunshine on 15/9/21.
  */
 public interface ReaderDao {
+    public ResultData insertReader(Reader reader);
+
+    public ResultData queryReader(Reader reader);
 }
--- src/main/java/platform/sunshine/dao/impl/ReaderDaoImpl.java
+++ src/main/java/platform/sunshine/dao/impl/ReaderDaoImpl.java
@@ -1,10 +1,24 @@
 package platform.sunshine.dao.impl;
 
 import platform.sunshine.dao.ReaderDao;
+import platform.sunshine.model.Reader;
 import platform.sunshine.utils.BaseDao;
+import platform.sunshine.utils.ResultData;
 
 /**
  * Created by sunshine on 15/9/21.
  */
 public class ReaderDaoImpl extends BaseDao implements ReaderDao {
+
+    @Override
+    public ResultData insertReader(Reader reader) {
+        ResultData result = new ResultData();
+        return result;
+    }
+
+    @Override
+    public ResultData queryReader(Reader reader) {
+        ResultData result = new ResultData();
+        return result;
+    }
 }
